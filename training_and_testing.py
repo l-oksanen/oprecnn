73d92bd8c94ad51cba46055f75ab5e1ad8d80dca
Aili Joutsela <aili.joutsela@helsinki.fi>
2022-05-04 20:48:31 +0300

    Modify diagonal element generator

--- training_and_testing.py
+++ training_and_testing.py
@@ -7,14 +7,21 @@ import time
 import opnet
 from simple_inversion_data import generate_data, save_data, load_data
 
-PATH = './simple_inversion_net2.pth'
+PATH = './simple_inversion_netPlusMinusReLU.pth'
+# PATH = './simple_inversion_netNEGA.pth'
 # PATH = './simple_inversion_netReLU.pth'
 
 def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr):
     # Load the training data
     train_loader = torch.utils.data.DataLoader(
-    load_data("simple_inversion_train_data.npz"), 
+    load_data("simple_inversion_train_dataPOSNEG.npz"), 
     batch_size=64)
+
+    # dataiter1 = iter(train_loader)
+    # a, b = dataiter1.next()
+    # print("matriisin ominaisarvot ovat")
+    # tulostetaan kääntämätön matriisi ominaisarvotarkastelua varten
+    # print(a[:2]) #[:2] tarkoittaa, että listan alusta tulostetaan 2 ekaa alkiota, eli kaksi matriisia
 
     # Learning rate parameter is from the quickstart guide 
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
@@ -23,11 +30,11 @@ def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr
     #otetaan aikaa kauan koko hommaan menee aikaa
     start_time=time.perf_counter()
 
-    for x in range(15):
+    for x in range(10):
         print("kierros ", x+1)
-        #mitataan looppiin kulunut aika
+        # measure the time consumed in epoch
         epoch_start = time.perf_counter()
-        for epoch in range(15): 
+        for epoch in range(10): 
             # print(f"Epoch {epoch+1}\n-------------------------------")
             for batch, (X, y) in enumerate(train_loader):
                 # Compute prediction error
@@ -46,27 +53,27 @@ def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr
         # print(f"epochiin kulunut aika: {epoch_end - epoch_start:0.4}")
         # str(datetime.timedelta(seconds=666))
 
-        #mitataan testaamiseen kulunut aika
+        # measure time consumed in testing
         testing_start = time.perf_counter()
 
         torch.save(model.state_dict(), PATH)
-
+    
         # Load trained variables
         model.load_state_dict(torch.load(PATH))
 
         # Load the testing data
         test_loader = torch.utils.data.DataLoader(
-        load_data("simple_inversion_test_data.npz"), 
+        load_data("simple_inversion_test_dataPOSNEG.npz"), 
         batch_size=64)
 
         dataiter = iter(test_loader)
         X, y = dataiter.next()
         with torch.no_grad():
             pred = model(X)
-        # print("True: ")
-        # print(y[:2])
-        # print("Prediction: ")
-        # print(pred[:2])
+        print("True: ")
+        print(y[:2])
+        print("Prediction: ")
+        print(pred[:2])
 
         num_batches = len(test_loader)
         test_loss = 0
