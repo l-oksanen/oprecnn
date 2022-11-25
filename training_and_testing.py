228155c9799d28ad4c51b8cad7723d60d144b828
Aili Joutsela <aili.joutsela@helsinki.fi>
2022-11-25 21:50:03 +0200

    Change code: Train multiple networks at one run to find best lr

--- training_and_testing.py
+++ training_and_testing.py
@@ -7,10 +7,6 @@ import time
 import opnet
 from simple_inversion_data import generate_data, save_data, load_data
 
-PATH = './simple_inversion_net4.pth'
-# PATH = './simple_inversion_netPlusMinusReLU.pth'
-# PATH = './simple_inversion_netNEGA.pth'
-# PATH = './simple_inversion_netReLU.pth'
 
 def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr, PATH, train_data_path, test_data_path):
     losses = [] #to save all the avg losses
@@ -29,17 +25,19 @@ def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr
     # Learning rate parameter is from the quickstart guide 
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
     print ("lr= ", lr)
+    print("path= ",PATH)
 
     #otetaan aikaa kauan koko hommaan menee aikaa
     #start_time=time.perf_counter()
 
     # we want the code to calculate the average loss until the point where
     # the average loss is under certain given point, "limit point", that is small enough
-    limit_point = 9e-5
+    limit_point = 9e-3
     avg_loss_now = 1 # average loss at this round. Gave some random big number for starters
     loop_counter = 0
 
-    while (avg_loss_now)>limit_point:
+    for x in range(25): #for-loop to find the best learning rate
+    #while (avg_loss_now)>limit_point: #while-loop to compare relu and no-relu
         loop_counter+=1
         # measure the time consumed in epoch
         #epoch_start = time.perf_counter()
@@ -58,7 +56,7 @@ def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr
                 #    n, N = (batch + 1) * len(X), len(train_loader.dataset)
                 #    print(f"loss: {loss.item():>7f}  [{n:>5d}/{N:>5d}]")
 
-        epoch_end = time.perf_counter()
+        #epoch_end = time.perf_counter()
         # print(f"epochiin kulunut aika: {epoch_end - epoch_start:0.4}")
         # str(datetime.timedelta(seconds=666))
 
@@ -66,7 +64,6 @@ def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr
         #testing_start = time.perf_counter()
 
         torch.save(model.state_dict(), PATH)
-    
         # Load trained variables
         model.load_state_dict(torch.load(PATH))
 
@@ -93,8 +90,9 @@ def training_and_testing(model: opnet.OperatorNet, loss_fn: torch.nn.MSELoss, lr
         test_loss /= num_batches
         avg_loss_now = test_loss
 
-        losses.append(test_loss) #adds a loss to the list of losses
-        print(f"{loop_counter*2} {test_loss:>8f}")
+        losses.append(test_loss) #adds a avg loss to the list of losses
+        print(f"{(x+1)*2} {test_loss:>8f}") #prints how many epochs has gone + avg loss
+        #print(f"{loop_counter*2} {test_loss:>8f}") #prints how many epochs has gone + avg loss
 
         #testing_end = time.perf_counter()
         # print(f"testaamiseen kulunut aika: {testing_end - testing_start:0.4} \n")
